0ad7e5150c268bb81948e22a84efd4891ebb02a9
mhbCO <mohamedhabib.bradai@componize.com>
2021-05-25 23:40:28 +0200

    first commit

---
+++ src/server.ts
@@ -0,0 +1,16 @@
+import express, {Request, Response} from 'express'
+import bodyParser from 'body-parser'
+
+const app: express.Application = express()
+const address: string = "0.0.0.0:3000"
+const port: number = 3000;
+
+app.use(bodyParser.json())
+
+app.get('/', function (req: Request, res: Response) {
+    res.send('Hello World!')
+})
+
+app.listen(port, function () {
+    console.log(`starting app on: ${address}`)
+})
